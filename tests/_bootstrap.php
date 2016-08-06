847c668a6c89f949a6eb03b0e51ac06a078937af
Andrii Vasyliev <sol@hiqdev.com>
2016-08-07 00:56:31 +0300

    inited empty tests

---
+++ tests/_bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+error_reporting(E_ALL);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
---
+++ tests/unit/ModuleTest.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * Cart module for Yii2
+ *
+ * @link      https://github.com/hiqdev/yii2-language
+ * @package   yii2-language
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hiqdev\yii2\language\tests\unit;
+
+use hiqdev\yii2\language\Module;
+use Yii;
+use yii\web\Application;
+
+/**
+ * Module test suite.
+ */
+class ModuleTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Module
+     */
+    protected $object;
+
+    protected function setUp()
+    {
+        $application = new Application([
+            'id'       => 'mock',
+            'basePath' => dirname(dirname(__DIR__)),
+            'modules'  => [
+                'language' => [
+                    'class' => Module::class,
+                ],
+            ],
+            'components' => [
+                'urlManager' => [
+                    'enablePrettyUrl' => true,
+                    'showScriptName'  => false,
+                ],
+            ],
+        ]);
+        $this->object = Module::getInstance();
+    }
+
+    protected function tearDown()
+    {
+    }
+
+    public function testGetInstance()
+    {
+        $module = Module::getInstance();
+        $this->assertInstanceOf(Module::class, $module);
+        $this->assertSame($this->object, $module);
+    }
+
+}
