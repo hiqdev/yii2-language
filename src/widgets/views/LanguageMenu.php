9079f753b2d50f5386bb3689c4487b2b8245bbea
tofid <andreyklochok@gmail.com>
2016-08-09 14:14:20 +0300

    missing namespace error

--- src/widgets/views/LanguageMenu.php
+++ src/widgets/views/LanguageMenu.php
@@ -3,20 +3,26 @@
 use yii\helpers\Html;
 use yii\helpers\Url;
 
+/* @var array $languages */
+/* @var string $language */
+/* @var string $selectUrl */
+
 ?>
 <li class="dropdown language-menu">
     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
         <?= strtoupper(substr($language, 0, 2)) ?>
     </a>
     <ul class="dropdown-menu">
-        <?php foreach ($languages as $code => $lang): ?>
-            <li class="header">
-                <?= Html::a(
-                    $lang,
-                    Url::to([$selectUrl, 'language' => $code]),
-                    ['class' => mb_stristr($language, $code) ? 'text-bold' : '']
-                ) ?>
-            </li>
-        <?php endforeach ?>
+        <?php if (is_array($languages)) : ?>
+            <?php foreach ($languages as $code => $lang): ?>
+                <li class="header">
+                    <?= Html::a(
+                        $lang,
+                        Url::to([$selectUrl, 'language' => $code]),
+                        ['class' => mb_stristr($language, $code) ? 'text-bold' : '']
+                    ) ?>
+                </li>
+            <?php endforeach ?>
+        <?php endif ?>
     </ul>
 </li>
