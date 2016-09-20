45a0e4c5b94d76e968faef56f8a439e1503b6d1f
tofid <andreyklochok@gmail.com>
2016-09-20 19:38:22 +0300

    minor

--- src/widgets/views/LanguageMenu.php
+++ src/widgets/views/LanguageMenu.php
@@ -8,21 +8,19 @@ use yii\helpers\Url;
 /* @var string $selectUrl */
 
 ?>
-<li class="dropdown language-menu">
-    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-        <?= strtoupper(substr($language, 0, 2)) ?>
-    </a>
-    <ul class="dropdown-menu">
-        <?php if (is_array($languages)) : ?>
-            <?php foreach ($languages as $code => $lang): ?>
-                <li class="header">
-                    <?= Html::a(
-                        $lang,
-                        Url::to([$selectUrl, 'language' => $code]),
-                        ['class' => mb_stristr($language, $code) ? 'text-bold' : '']
-                    ) ?>
-                </li>
-            <?php endforeach ?>
-        <?php endif ?>
-    </ul>
-</li>
+<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+    <?= strtoupper(substr($language, 0, 2)) ?>
+</a>
+<ul class="dropdown-menu">
+    <?php if (is_array($languages)) : ?>
+        <?php foreach ($languages as $code => $lang): ?>
+            <li class="header">
+                <?= Html::a(
+                    $lang,
+                    Url::to([$selectUrl, 'language' => $code]),
+                    ['class' => mb_stristr($language, $code) ? 'text-bold' : '']
+                ) ?>
+            </li>
+        <?php endforeach ?>
+    <?php endif ?>
+</ul>
