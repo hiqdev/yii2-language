f43212be9e123659b0922556bcbbb3673204940b
Andrii Vasyliev <sol@hiqdev.com>
2017-04-19 04:15:13 +0000

    hidden LanguageMenu when no other languages

--- src/widgets/views/LanguageMenu.php
+++ src/widgets/views/LanguageMenu.php
@@ -6,18 +6,20 @@ use yii\helpers\Html;
 /* @var string $language */
 
 ?>
-<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-    <?= strtoupper(substr($language, 0, 2)) ?>
-</a>
-<ul class="dropdown-menu">
-    <?php if (is_array($items)) : ?>
-        <?php foreach ($items as $code => $item): ?>
-            <li class="header">
-                <?= Html::a(
-                    $item['label'], $item['url'],
-                    ['class' => empty($item['active']) ? '' : 'text-bold']
-                ) ?>
-            </li>
-        <?php endforeach ?>
-    <?php endif ?>
-</ul>
+<?php if (count($items)>1) : ?>
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+        <?= strtoupper(substr($language, 0, 2)) ?>
+    </a>
+    <ul class="dropdown-menu">
+        <?php if (is_array($items)) : ?>
+            <?php foreach ($items as $code => $item): ?>
+                <li class="header">
+                    <?= Html::a(
+                        $item['label'], $item['url'],
+                        ['class' => empty($item['active']) ? '' : 'text-bold']
+                    ) ?>
+                </li>
+            <?php endforeach ?>
+        <?php endif ?>
+    </ul>
+<?php endif ?>
